f3ad9646e5903ebecf34638148d28c3283203dfa
AdilHannaoui <154902011+AdilHannaoui@users.noreply.github.com>
2023-12-31 11:50:48 +0100

    submit_blast_singul_hpcbio-pacioli_alumno07.sh

--- blast/submit_blast_singul_hpcbio-pacioli_alumno07.sh
+++ blast/submit_blast_singul_hpcbio-pacioli_alumno07.sh
@@ -2,7 +2,6 @@
 #SBATCH -p hpc-bio-pacioli
 #SBATCH --chdir=/home/alumno07/
 #SBATCH -J lab6
-#SBATCH --ntasks-per-node=4  # a maximum count of tasks per node
 #SBATCH --cpus-per-task=4    # number of processors per task
 #SBATCH --mail-type=NONE    #END/START/NONE
 #SBATCH --mail-user=MAIL@um.es
